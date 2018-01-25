a33f77e76736fead4ff2688037cc6819107a61d5
Tessa Kriesel <me@tessak22.com>
2018-01-25 15:08:08 -0600

    Remove test comment files

--- blocks/three_column_services.php
+++ blocks/three_column_services.php
@@ -5,34 +5,36 @@
 ?>
 
 <section class="content-block three_columns_services container">
-		<h2 class="section-title text-center"><?php echo $sectiontitle; ?></h2>
-		<p class="text-center"><?php echo $sectiondescription; ?></p>
+	<h2 class="section-title text-center"><?php echo $sectiontitle; ?></h2>
+	<p class="text-center"><?php echo $sectiondescription; ?></p>
 
-		<div class="row">
-			<?php
-			// loop through the repeater content
-			while( have_rows('service_items') ): the_row(); 
-				$title = get_sub_field('title');
-				$image = get_sub_field('image');
-				$category = get_sub_field('category');
-				$link = get_sub_field('link');
-			?>
-			<div class="three_columns_services_item col-sm-4 text-center">
-				<?php if ( $title ): ?>
-					<h3><?php echo $title; ?></h3>
-				<?php endif; ?>
+	<div class="row">
 
-				<?php if ( $category ): ?>
-					<p><?php echo $category; ?></p>
-				<?php endif; ?>
+		<?php
+		// loop through the repeater content
+		while( have_rows('service_items') ): the_row(); 
+			$title = get_sub_field('title');
+			$image = get_sub_field('image');
+			$category = get_sub_field('category');
+			$link = get_sub_field('link');
+		?>
+		
+		<div class="three_columns_services_item col-sm-4 text-center">
+			<?php if ( $title ): ?>
+				<h3><?php echo $title; ?></h3>
+			<?php endif; ?>
 
-				<?php if ( $image ): ?>
-					<a href="<?php echo $link; ?>"><img class="img-responsive" src="<?php echo $image; ?>" alt="<?php echo $title; ?>"></a>
-				<?php endif; ?>
-			</div>
+			<?php if ( $category ): ?>
+				<p><?php echo $category; ?></p>
+			<?php endif; ?>
+
+			<?php if ( $image ): ?>
+				<a href="<?php echo $link; ?>"><img class="img-responsive" src="<?php echo $image; ?>" alt="<?php echo $title; ?>"></a>
+			<?php endif; ?>
+		</div>
 
 		<?php endwhile; ?>
 
-		</div>
+	</div>
 
 </section>
